c6dd57b5e7462476c7e6638bb5daf6ec16333027
Raghu Betina <raghu@firstdraft.com>
2016-11-14 20:21:40 +0000

    Whitelist columns in Shop for ActiveAdmin

--- app/admin/shop.rb
+++ app/admin/shop.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Shop do
 
+ permit_params :name, :address, :hours
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
